b151e0f17b2e9ed383601cd888e0dee7219e3db4
Charlie Summers <charlie@gomerits.com>
2024-02-01 12:08:27 -0800

    Update test.py
    smokedduck.connect() -> smokedduck.prov_connect()
    
    Signed-off-by: Charlie Summers <charlie@gomerits.com>

--- smokedduck/test.py
+++ smokedduck/test.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 p1 = pd.DataFrame({'a': [42, 43, 44, 45], 'b': ['a', 'b', 'a', 'b']})
 p2 = pd.DataFrame({'b': ['a', 'a', 'c', 'b'], 'c': [4, 5, 6, 7]})
-con = smokedduck.connect(':default:')
+con = smokedduck.prov_connect(':default:')
 con.execute('create table t1 as (select * from p1)')
 con.execute('create table t2 as (select * from p2)')
 df = con.execute('SELECT t1.b, sum(a + c) FROM t1 join (select b, avg(c) as c from t2 group by b) as t2 on t1.b = t2.b group by t1.b', capture_lineage='lineage').df()
